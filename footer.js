245bfda6739d41e8f50ba9cf6f8a2e9e33ab752e
Surendra Agarwal <surendra-@users.noreply.github.com>
2018-01-27 18:37:47 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <h2>THis is footer</h2>
+      </footer>
+    );
+  }
+}
